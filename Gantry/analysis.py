bdb9942b0d57ba926fb8ebcf0d517b00d84c19c4
RaymondDunn <Ray.L.Dunn@gmail.com>
2019-05-27 16:56:04 -0700

    bug fixes

--- Gantry/analysis.py
+++ Gantry/analysis.py
@@ -1,7 +1,8 @@
 import csv
+import os
 
 # get data
-p1_filename = "local data filename here"
+p1_filename = "measurelist_2019_05_22_13_29_34.csv"
 
 # get path info
 cwd = os.getcwd()
@@ -19,18 +20,31 @@ with open(p1_path) as f:
 
 	for line in fcontent:
 
-		# append formatted list
-		formattedline = line[1:-1]		# remove brackeds
-		splitline = line.split(',')		# split on commas
-		val = int(splitline[0])
-		gain = int(splitline[1])
-		x = int(splitline[2])
-		y = int(splitline[3])
+		# split line into the actual tuples of data
+		data = line.split('\"')
 
-		# store vals
-		measurements.append(val)
-		gains.append(gain)
-		xs.append(x)
-		ys.append(y)
+		# ugh this is so gross
+		for d in data:
+			if len(d) > 1:
+
+				# formatting
+				chars_to_replace = ['[', ']', '(', ')', '\'']
+				formattedline = d
+				for char in chars_to_replace:
+					formattedline = formattedline.replace(char, '')
+
+
+				# append formatted list
+				splitline = formattedline.split(',')		# split on commas
+				val = int(splitline[0])
+				gain = int(splitline[1])
+				x = int(splitline[2])
+				y = int(splitline[3])
+
+				# store vals
+				measurements.append(val)
+				gains.append(gain)
+				xs.append(x)
+				ys.append(y)
 
 # plot however we want
